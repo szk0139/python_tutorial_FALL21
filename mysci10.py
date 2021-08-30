ee08d71958674f3a35ab0eabe98c66cd57e3b976
Sanjiv Kumar <kumar34@cheyenne5.cheyenne.ucar.edu>
2021-08-30 11:24:51 -0600

    reading windspeed in too

---
+++ mysci10.py
@@ -0,0 +1,64 @@
+
+#Column names and column indices to read
+
+columns = {'date':0, 'time': 1, 'tempout':2, 'windspeed':7}
+
+# Data types for each column (only if non-string)
+
+types = {'timeout': float, 'windspeed':float}
+
+#Initialize my data variable
+data = {}
+for column in columns:
+    data[column] = []
+
+
+# Initialize my data variable
+#data = {'date':[], 'time':[], 'tempout':[]}
+#time = data['time']
+
+
+# Reading the data file
+filename = "./data/wxobs20170821.txt"
+
+with open(filename, "r") as datafile:
+    # read first three line (header)
+    for _ in range(3):
+        #print(_)
+        datafile.readline()
+        
+ 
+    # Read and parse the rest of the file
+    for line in datafile:
+        split_line = line.split()
+        for column in columns:
+            i = columns[column]
+            t = types.get(column, str)
+            value = t(split_line[i])
+            data[column].append(value)
+ 
+
+# DEBUG
+#print(data['tempout'])
+
+
+
+
+#DBUG
+#for datum in data:
+#    print(datum)
+#print(data[0])
+#print(data[9])
+#print(data[-1])
+#print(data[0:10])
+#for datum in data[0:10]:
+#    print(datum)
+#for datum in data[:10:2]:
+#    print(datum)
+#print(data[8][4])
+#print(data[8][4][0])
+#print(data[8][:5])
+#print(data[8][:5:2])
+#print(data[5:8][0])
+#print(data[5])
+
