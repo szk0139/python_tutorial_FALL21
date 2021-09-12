92db1e33c36061774f84c9a0777748575dbc97ce
Sanjiv Kumar <kumar34@cheyenne1.cheyenne.ucar.edu>
2021-09-12 14:58:01 -0600

    adding setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(
     version="1.0.0",
     description="A sample package",
     author= "Sanjiv Kumar",
-    author_email="szk0139@auburn.edu,
+    author_email="szk0139@auburn.edu",
     packages=["mysciW4"],
     install_requires=[])
