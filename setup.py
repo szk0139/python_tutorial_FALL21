02835df57459ce3f535291de0abc119c62ef146c
Sanjiv Kumar <kumar34@cheyenne1.cheyenne.ucar.edu>
2021-09-12 14:34:38 -0600

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mysciW4",
+    version="1.0.0",
+    description="A sample package",
+    author= "Sanjiv Kumar",
+    author_email="szk0139@auburn.edu,
+    packages=["mysciW4"],
+    install_requires=[])
